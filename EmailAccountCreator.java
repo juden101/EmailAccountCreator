9572bfbe4b6a72fa22e630b6dcb5cf356a1ef037
jude <nelsonjudith@hotmail.com>
2020-08-23 13:00:24 +0100

    Initial sharing of project

---
+++ EmailAccountCreator.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class EmailAccountCreator here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class EmailAccountCreator
+{
+    //main method when project runs
+    public static void main(String[] args)
+    {
+        // put your code here
+        
+    }
+    
+    public static void menu(){
+        //put your menu code here
+    }
+    
+    public static void createEmail(String username){
+    
+    }
+}
